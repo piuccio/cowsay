21865b2a6f68924996a748b29b5b941a99b116ea
iArmann <mrevid2@gmail.com>
2020-08-10 19:07:09 +0430

    Added documentation

--- index.js
+++ index.js
@@ -2,14 +2,129 @@ var baloon = require("./lib/balloon");
 var cows = require("./lib/cows");
 var faces = require("./lib/faces");
 
+/**
+ * @param options
+ * ## Face :
+ * Either choose a mode (set the value as true) **_or_**
+ * set your own defined eyes and tongue to `e` and `T`.
+ * - ### `e` : eyes
+ * - ### `T` : tongue
+ * 
+ * ## Cow :
+ * Either specify a cow name (e.g. "fox") **_or_**
+ * set the value of `r` to true which selects a random cow.
+ * - ### `r` : random selection
+ * - ### `f` : cow name - from `cows` folder
+ * 
+ * ## Modes :
+ * Modes are just ready-to-use faces, here's their list:
+ * - #### `b` : borg
+ * - #### `d` : dead      
+ * - #### `g` : greedy
+ * - #### `p` : paranoia
+ * - #### `s` : stoned
+ * - #### `t` : tired
+ * - #### `w` : youthful
+ * - #### `y` : wired
+ * 
+ * @example
+ * ```
+ * // custom cow and face
+ * cowsay.say({
+ *     text: 'Hello world!',
+ *     e: '^^', // eyes
+ *     T: 'U ', // tongue
+ *     f: 'USA' // name of the cow from `cows` folder
+ * })
+ * 
+ * // using a random cow
+ * cowsay.say({
+ *     text: 'Hello world!',
+ *     e: 'xx', // eyes
+ *     r: true, // random mode - use a random cow.
+ * })
+ * 
+ * // using a mode
+ * cowsay.say({
+ *     text: 'Hello world!',
+ *     y: true, // using y mode - youthful mode
+ * })
+ * ```
+ */
 exports.say = function (options) {
 	return doIt(options, true);
 };
 
+/**
+ * @param options
+ * ## Face :
+ * Either choose a mode (set the value as true) **_or_**
+ * set your own defined eyes and tongue to `e` and `T`.
+ * - ### `e` : eyes
+ * - ### `T` : tongue
+ * 
+ * ## Cow :
+ * Either specify a cow name (e.g. "fox") **_or_**
+ * set the value of `r` to true which selects a random cow.
+ * - ### `r` : random selection
+ * - ### `f` : cow name - from `cows` folder
+ * 
+ * ## Modes :
+ * Modes are just ready-to-use faces, here's their list:
+ * - #### `b` : borg
+ * - #### `d` : dead      
+ * - #### `g` : greedy
+ * - #### `p` : paranoia
+ * - #### `s` : stoned
+ * - #### `t` : tired
+ * - #### `w` : youthful
+ * - #### `y` : wired
+ * 
+ * @example
+ * ```
+ * // custom cow and face
+ * cowsay.think({
+ *     text: 'Hello world!',
+ *     e: '^^', // eyes
+ *     T: 'U ', // tongue
+ *     f: 'USA' // name of the cow from `cows` folder
+ * })
+ * 
+ * // using a random cow
+ * cowsay.think({
+ *     text: 'Hello world!',
+ *     e: 'xx', // eyes
+ *     r: true, // random mode - use a random cow.
+ * })
+ * 
+ * // using a mode
+ * cowsay.think({
+ *     text: 'Hello world!',
+ *     y: true, // using y mode - youthful mode
+ * })
+ * ```
+ */
 exports.think = function (options) {
 	return doIt(options, false);
 };
 
+/**
+ * @param callback
+ * @returns a list of cow names from the cows folder without the .cow extension.
+ * @example
+ * ```
+ * function callback(error, cow_names) {
+ *      if (error) {
+ *          console.log(error);
+ *      }
+ *      else if (cow_names) {
+ *          console.log('Cows: ' + cow_names.length);
+ *      }
+ * }
+ * 
+ * cowsay.list(callback);
+ * ```
+ */
 exports.list = cows.list;
 
 function doIt (options, sayAloud) {
