71d1dfaea3147f6c9f043730b5b9221298c3461c
Fabio Crisci <piuccio@gmail.com>
2020-08-24 20:57:00 +0900

    pretty

--- test/cli.js
+++ test/cli.js
@@ -3,18 +3,18 @@ const execa = require('execa');
 
 test('say something', async (t) => {
   t.plan(1);
-  const {stdout} = await execa.node('cli.js', ['say']);
+  const { stdout } = await execa.node('cli.js', ['say']);
   t.match(stdout, /< say >/);
 });
 
 test('think something', async (t) => {
   t.plan(1);
-  const {stdout} = await execa.node('cli.js', ['--think', 'something']);
+  const { stdout } = await execa.node('cli.js', ['--think', 'something']);
   t.match(stdout, /( something )/);
 });
 
 test('from stdin', async (t) => {
   t.plan(1);
-  const {stdout} = await execa.node('cli.js', {input: 'echo'});
+  const { stdout } = await execa.node('cli.js', { input: 'echo' });
   t.match(stdout, /< echo >/);
 });
